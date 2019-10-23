69718b81773d3a36083baf70b7007801388a850e
João Prates <jvdprates@hotmail.com>
2019-10-23 17:54:44 -0300

    Esqueleto Inicial
    Neste commit estou adicionando os arquivos relacionados as 4 paginas do nosso sistema web.

--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Página Inicial' });
 });
+
+router.get('/1aba', function(req, res, next) {
+  res.render('1aba', { title: 'Primeira aba' });
+});
+
+router.get('/2aba', function(req, res, next) {
+  res.render('2aba', { title: 'Segunda aba' });
+});
+
+router.get('/3aba', function(req, res, next) {
+  res.render('3aba', { title: 'Terceira aba' });
+});
+
+
+
+
 
 module.exports = router;
