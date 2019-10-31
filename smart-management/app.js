15254ef6aa83eeaf92526a40b8bae7d4f09917cd
Lucas Xavier <lucasxavier@cpejr.com.br>
2019-10-31 10:05:06 -0300

    colocando firebase setup (const config)

--- smart-management/app.js
+++ smart-management/app.js
@@ -12,6 +12,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 
+/**
+ * firebase setup
+ */
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
